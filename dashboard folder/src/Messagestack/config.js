ab7080523c7437514f4a0097a9b2b5e96d059abb
Sourabh Misal <sourabhmisal1@gmail.com>
2019-04-14 08:24:50 +0530

    Add files via upload

---
+++ dashboard folder/src/Messagestack/config.js
@@ -0,0 +1,9 @@
+export default {
+    // MQTT servers websocket connection
+    port: 30968,
+    host: 'm16.cloudmqtt.com',
+    username: 'srfmjxuy',
+    password: 'GkMPS1T5F8d4',
+    clientId: 'bledemo_' + Math.random().toString(16).substr(2, 8),
+    channel: '/beacons/office'
+};
