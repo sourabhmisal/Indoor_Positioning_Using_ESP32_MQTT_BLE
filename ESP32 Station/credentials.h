bfe28ac2be2cceafb2a68bac913bd1ec400e6375
Sourabh Misal <sourabhmisal1@gmail.com>
2019-04-24 16:25:10 +0530

    Add files via upload

---
+++ ESP32 Station/credentials.h
@@ -0,0 +1,13 @@
+#ifndef CREDENTIALS_H_
+#define CREDENTIALS_H_
+/* WiFi username and password */
+const char* ssid = "SM";
+const char* password = "12345678901";
+
+/* MQTT credentials and connection */
+const char* mqttServer = "192.168.43.205"   ;
+const int mqttPort = 1883;
+const char* mqttUser = "station";
+const char* mqttPassword = "bledemo";
+
+#endif
